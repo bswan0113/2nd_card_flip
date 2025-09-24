bbc9d219c734b23fd132fa36aef15007b9616b3d
p99275751-ai <p99275751@gmail.com>
2025-09-24 09:22:15 +0900

    자물쇠 복구
    자물쇠복구 및 엔딩화면 추가중

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
@@ -20,6 +21,27 @@ public class UIManager : MonoBehaviour
     }
     public void OpenStage()
     {
-        
+        //스테이지1,2 클리어시 작동
+        if (stagePn != null)
+        {
+            stagePn.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("아래스테이지가 클리어되지 않았습니다");
+        }
+    }
+    public void Ending()
+    {
+        //3스테이지 클리어시 엔딩화면
+        if (end != null)
+        {
+            end.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("아래스테이지가 클리어되지 않았습니다");
+        }
     }
 }
+
