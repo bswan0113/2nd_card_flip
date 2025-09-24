bb2f90adceb9592ba13bff1f402522af54584be4
chungsim <zoowon00@naver.com>
2025-09-24 18:07:18 +0900

    메인 씬 연출 병합 작업
    1. 게임 메니저 수정 사항
     a. 연출 중복 재생 방지 bool 조건 추가
     b. 남은 시간 10초의 기준을 스테이지 시간제한 - 현재 시간으로 변경
     c. selectStageContainer를 public으로 변경
     d. 화면 흔들림, 먼지 연출 추가
    
    2. 메인씬 수정 사항
     a. EffectManager 추가
     b. 게임시작 버튼에 연결된 함수를 이팩트 매니저의 GameStartRoutine으로 변경 -> 게임시작시 3초 대기 연출

--- Assets/Scripts/EffectManager.cs
+++ Assets/Scripts/EffectManager.cs
@@ -20,7 +20,6 @@ public class EffectManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
